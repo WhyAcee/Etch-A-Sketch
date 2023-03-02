f09d57dd5d182a4c2e2b33d22503ccb461c973db
Marlon <marlonwason100@gmail.com>
2023-03-01 21:24:20 -0500

    add a slider for the grid size

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const gridContainer = document.querySelector('.grid-container')
 const gridItem = document.querySelector('.grid-item')
 
-let gridSize = 16; 
+let gridSides = 16; 
 // Creates a 16x16 grid
 function createGrid() {
     clearGrid()
-    for(let i = 0; i < (gridSize * gridSize); i++) {
+    gridContainer.style.gridTemplateColumns = (`repeat(${gridSides}, 1fr)`)
+    gridContainer.style.gridTemplateRows = (`repeat(${gridSides}), 1fr)`)
+
+    console.log(gridSides)
+    for(let i = 0; i < (gridSides * gridSides); i++) {
         const div = document.createElement('div')
         div.classList.add('grid-item')
         div.addEventListener('mouseover', function(e) {
@@ -21,11 +25,22 @@ createGrid();
     while (gridContainer.firstChild)
         gridContainer.removeChild(gridContainer.firstChild)
     }
+
 //Buttons:
 //Clear button
 const clearBtn = document.querySelector('#clear')
 clearBtn.addEventListener('click', function(e) {
-    clearGrid()
     createGrid()
 });
- 
+
+//slider for number of sides
+const sliderInput = document.querySelector('#slider-input')
+const sliderValue = document.querySelector('#slider-value')
+//sliderValue.textContent = sliderValue.sliderInput
+
+sliderInput.addEventListener("input", function(e) {
+    sliderValue.textContent = e.target.value
+    console.log(e.target.value)
+    return gridSides = e.target.value, createGrid()
+})
+
