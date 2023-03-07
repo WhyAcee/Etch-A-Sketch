3991144246076bf9794af3f88cea7fac5004aa62
Marlon <marlonwason100@gmail.com>
2023-03-06 21:08:37 -0500

    fix color changer

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let gridSides = 16;
 // Creates a 16x16 grid
 function createGrid() {
     clearGrid()
-    gridContainer.style.gridTemplateColumns = (`repeat(${gridSides}, 1fr)`)
-    gridContainer.style.gridTemplateRows = (`repeat(${gridSides}), 1fr)`)
+    gridContainer.style.gridTemplateColumns = (`repeat(${gridSides}, ${600 / gridSides}px)`)
+    gridContainer.style.gridTemplateRows = (`repeat(${gridSides}), ${600 / gridSides}px)`)
 
-    console.log(gridSides)
     for(let i = 0; i < (gridSides * gridSides); i++) {
         const div = document.createElement('div')
         div.classList.add('grid-item')
+        div.style.padding = `${200 / gridSides}px`
         div.addEventListener('mouseover', function(e) {
-            e.target.style.backgroundColor = "black";
+            e.target.style.backgroundColor = `${defaultColor}`;
         })
         gridContainer.appendChild(div)
     }
@@ -33,10 +33,17 @@ clearBtn.addEventListener('click', function(e) {
     createGrid()
 });
 
+//Color picker
+const colorPicker = document.querySelector('#color-picker')
+let defaultColor = colorPicker.value
+
+colorPicker.addEventListener("input", function(e) {
+    return defaultColor = e.target.value
+})
+
 //slider for number of sides
 const sliderInput = document.querySelector('#slider-input')
 const sliderValue = document.querySelector('#slider-value')
-//sliderValue.textContent = sliderValue.sliderInput
 
 sliderInput.addEventListener("input", function(e) {
     sliderValue.textContent = e.target.value
